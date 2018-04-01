2e12e19c9d74cb3509ffbbbc6b1d79aaf6702d26
Tony Pagliocco <tpagliocco@users.noreply.github.com>
2018-03-31 22:12:44 -0700

    Create Find_2nd_Highest_Element.py

---
+++ Easy Python Practice/Find_2nd_Highest_Element.py
@@ -0,0 +1,24 @@
+#Take a integer N and then N inputs 
+#Find the second highest integer in the list
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    l = [ ]
+    for i in arr:
+      l.append(i)
+    
+    #print(l)
+    l.sort()
+    #print(l)
+    #print(l.count(max(l)))
+    y = len(l)
+    for i in l:
+        while l[-1] == l[-2]:
+            #print(l[-1])
+            #print(l[-2])
+        #if l[y-1] == l[y-2]:
+            l.pop()
+            #print(l)
+
+    print(l[-2])
